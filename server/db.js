12eaa5f2ed7e72cfa52ec1c59c6e9b072ff1c97e
Squareben1 <bengittins@live.com>
2020-07-15 19:18:53 +0100

    Added post method for users

--- server/db.js
+++ server/db.js
@@ -1,5 +1,13 @@
-const Pool = require("pg").Pool
+const Pool = require("pg").Pool;
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
 
 const pool = new Pool({
-  user: ""
-})
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  host: "localhost",
+  port: 5432,
+  database: "bujj_it",
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db")
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.post("/users", async (req, res) => {
+  try {
+    const { first_name, surname, username, dob, email, password_hash } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO users (first_name, surname, username, dob, email, password_hash) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [first_name, surname, username, dob, email, password_hash]
+    );
+
+    res.json(newUser.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
