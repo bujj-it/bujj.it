76cef0ae2ea460a42188fb8652d08b24fba0318d
Squareben1 <bengittins@live.com>
2020-07-17 12:42:31 +0100

    Controller for Authentication - controllers/auth.controller.js

---
+++ server/app/controllers/user.controller.js
@@ -0,0 +1,68 @@
+const db = require("../models");
+const config = require("../config/auth.config");
+const User = db.user;
+
+const Op = db.Sequelize.Op;
+
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+
+exports.signup = (req, res) => {
+  // Save User to Database
+  User.create({
+    first_name: req.body.first_name,
+    surname: req.body.surname,
+    username: req.body.username,
+    dob: req.body.dob,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+  }).catch((err) => {
+    res.status(500).send({ message: err.message });
+  });
+};
+
+exports.signin = (req, res) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "Invalid Password!",
+        });
+      }
+
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400, // 24 hours
+      });
+
+      var authorities = [];
+      user.getRoles().then((roles) => {
+        for (let i = 0; i < roles.length; i++) {
+          authorities.push("ROLE_" + roles[i].name.toUpperCase());
+        }
+        res.status(200).send({
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roles: authorities,
+          accessToken: token,
+        });
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
