0f39e6a3632e1f506fb0401a21239082707ce25f
Squareben1 <bengittins@live.com>
2020-07-17 12:50:25 +0100

    Controller for Authentication - controllers/auth.controller.js

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -47,20 +47,6 @@ exports.signin = (req, res) => {
       var token = jwt.sign({ id: user.id }, config.secret, {
         expiresIn: 86400, // 24 hours
       });
-
-      var authorities = [];
-      user.getRoles().then((roles) => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLE_" + roles[i].name.toUpperCase());
-        }
-        res.status(200).send({
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          accessToken: token,
-        });
-      });
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
