b6b4d8e58bc85948bbe6c62d02985aafea7b45c4
Squareben1 <bengittins@live.com>
2020-07-27 15:27:19 +0100

    Basic SCSS structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "./assets/chubby_budgie.jpeg";
-import "./styles/App.css";
+import logo from './assets/chubby_budgie.jpeg';
+import NavBar from './components/NavBar';
+import "./styles/App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <h1>bujj.it</h1>
         <br></br>
         <div className="blurb">Your new budgeting budgie buddy - coming soon!</div>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <>
+        <section className="navBar">Hello</section>
+      </>
+    );
+  }
+}
+
+export default NavBar;
