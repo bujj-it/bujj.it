0bd9b0669e462cc5604655e16fe42e4ea73687c9
cpcwood <cpcwood@outlook.com>
2020-07-27 13:56:43 +0100

    refactor routes

--- server/app/routes/auth.routes.js
+++ server/app/routes/auth.routes.js
@@ -1,13 +1,5 @@
 const authController = require("../controllers/auth.controller");
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-
   app.post("/api/auth", authController.signin);
 };
--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -3,14 +3,6 @@ const { authJwt } = require("../middleware");
 const userController = require("../controllers/user.controller");
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-  
   app.get(
     "/api/user",
     [authJwt.verifyToken],
