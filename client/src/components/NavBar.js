ea00b9085e97bb86a74c256b3da98d596adc6cea
cpcwood <cpcwood@outlook.com>
2020-07-30 16:59:57 +0100

    navTitle moves with scroll

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,14 +5,14 @@ import logo from "../assets/chubby_budgie.jpeg";
 
 class NavBar extends React.Component {
   render() {
-  
+ 
     return (
       <>
         <section className="navBarContainerSection">
           <div className="navBarContainerResponsive">
             <img src={logo} className="navLogo navItem" alt="bujj.it logo" />
-            <div className="navTitle navItem">
-              <NavTitle />
+            <div className="navItem">
+              <NavTitle location={this.props.location} />
             </div>
 
             <div className="navBurgerMenuContainer navItem">
--- client/src/components/NavTitle.js
+++ client/src/components/NavTitle.js
@@ -1,17 +1,64 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
-const NavTitle = () => {
-  let titleClass = "navTitle";
-  if (useLocation().pathname == "/") {
-    titleClass = "navTitle navTitleHomepage";
-    // add event listener for onscroll
+class NavTitle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.divRef = React.createRef()
+    this.state = {
+      baseTopOffset: null,
+      currentTopOffset: null,
+      baseFontSize: null,
+      currentFontSize: null
+    };
   }
-  return (
-    <>
-      <div className={titleClass}>bujj.it</div>
-    </>
-  );
+
+  componentDidMount = () => {
+    if (this.props.location.pathname == "/") {
+      const elementStyle = getComputedStyle(this.divRef.current) // gets all styles for current element 
+      const topOffset = parseInt(elementStyle.top, 10)
+      const fontSize = parseInt(elementStyle.fontSize, 10)
+      this.setState({
+        baseTopOffset: topOffset,
+        currentTopOffset: topOffset,
+        baseFontSize: fontSize,
+        currentFontSize: fontSize
+      })
+      window.addEventListener('scroll', this.handleScroll)
+    }
+  }
+  
+  componentWillUnmount = () => {
+    if (this.props.location.pathname == "/") {
+      window.removeEventListener('scroll', this.handleScroll)
+    }
+  }
+
+  handleScroll = (event) => {
+    const elementStyle = getComputedStyle(this.divRef.current)
+    const currentFontSize = parseInt(elementStyle.fontSize, 10)
+    // const newFontSize = try make this change size relative to max and min we like (Add data to element? Get from SASS? Get from somewhere?)
+    const scrollTop = event.srcElement.documentElement.scrollTop
+    const newTopOffset = Math.max(0, this.state.baseTopOffset - scrollTop)
+    this.setState({
+      currentTopOffset: newTopOffset
+    })
+  }
+  
+  render() {
+    let titleClass = "navTitle";
+
+    if ( this.props.location.pathname == "/") {
+      titleClass += " navTitleHomepage";
+    }
+
+    return (  
+      <>
+        <div className={titleClass} ref={this.divRef} style={{top: this.state.currentTopOffset, fontSize: this.state.currentFontSize}}>
+          bujj.it
+        </div>
+      </>
+    );
+  }
 };
 
 export default NavTitle;
