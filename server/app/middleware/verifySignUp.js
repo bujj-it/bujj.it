af1c8ae5609fee2115f37db4ec17e797a0a6a37f
cpcwood <cpcwood@outlook.com>
2020-09-16 19:50:10 +0100

    update verifySignUp#checkDuplicateUsernmeOrEmail to search dynamoDB instead of local database

--- server/app/middleware/verifySignUp.js
+++ server/app/middleware/verifySignUp.js
@@ -2,56 +2,50 @@ function checkDuplicateUsernameOrEmail(db) {
   const database = db.dynamoDb
   const userTable = db.users
 
-  return (req, res, next) => {
-    // Username
-    const params = {
-      TableName: userTable,
-      IndexName: 'usernameIndex',
-      KeyConditionExpression: '#username = :username',
-      ExpressionAttributeNames: {
-        "#username": "username"
-      },
-      ExpressionAttributeValues: {
-        ":username": req.body.username
+  return async (req, res, next) => {
+    try {
+      const searchForExistingUsername = {
+        TableName: userTable,
+        IndexName: 'usernameIndex',
+        KeyConditionExpression: '#username = :username',
+        ExpressionAttributeNames: {
+          "#username": "username"
+        },
+        ExpressionAttributeValues: {
+          ":username": req.body.username
+        }
       }
+      const usersWithUsername = await database.query(searchForExistingUsername).promise()
+      if (usersWithUsername.length > 0) {
+        return res.status(400).send({
+          message: "Failed! Username is already in use!"
+        });
+      }
+
+      const searchForExistingEmail = {
+        TableName: userTable,
+        IndexName: 'emailIndex',
+        KeyConditionExpression: '#email = :email',
+        ExpressionAttributeNames: {
+          "#email": "email"
+        },
+        ExpressionAttributeValues: {
+          ":email": req.body.email
+        }
+      }
+      const usersWithEmail = await database.query(searchForExistingEmail).promise()
+      if (usersWithEmail.length > 0) {
+        return res.status(400).send({
+          message: "Failed! Email is already in use!"
+        });
+      }
+
+      next();
+
+    } catch (err) {
+      console.log(err)
+      return res.status(500).send({message: 'Sorry something went wrong!'})
     }
-    
-    database.query(params, (err, data) => {
-      return res.status(200).send({
-        error: err,
-        data: data,
-        postBody: req.body
-      })
-    })
-    
-    // User.findOne({
-    //   where: {
-    //     username: req.body.username,
-    //   },
-    // }).then((user) => {
-    //   if (user) {
-    //     res.status(400).send({
-    //       message: "Failed! Username is already in use!",
-    //     });
-    //     return;
-    //   }
-
-    //   // Email
-    //   User.findOne({
-    //     where: {
-    //       email: req.body.email,
-    //     },
-    //   }).then((user) => {
-    //     if (user) {
-    //       res.status(400).send({
-    //         message: "Failed! Email is already in use!",
-    //       });
-    //       return;
-    //     }
-
-    //     next(); 
-    //   });
-    // });
   };
 }
 
