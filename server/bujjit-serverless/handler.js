635b3af4441d92a300d3785c1e8e6a9c751d99f4
Squareben1 <bengittins@live.com>
2020-09-16 15:41:15 +0100

    Deleted incorrect user-auth serverless dir

--- server/bujjit-serverless/handler.js
+++ server/bujjit-serverless/handler.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
+module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Successful YOO",
         input: event,
       },
       null,
