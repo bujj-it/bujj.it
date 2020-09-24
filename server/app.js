9acba9dc9be0d23c6a39cf98396815a1276bc803
Squareben1 <bengittins@live.com>
2020-09-24 18:10:53 +0100

    inject db to app to allow for testing of endpoints

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+module.exports = (db) => {
+  const app = express();
+
+  // parse requests of content-type - application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // parse requests of content-type - application/json
+  app.use(bodyParser.json());
+
+  // manage and sign cookies
+  app.use(cookieParser(process.env.SITE_SECRET));
+
+  require("./app/routes/users.routes")(app, db);
+  require("./app/routes/sessions.routes")(app, db);
+
+  app.get("/api", (req, res) => {
+    res.status(200).json({
+      message: "Welcome to bujj.it API",
+    });
+  });
+
+  return app;
+};
---
+++ server/server.js
@@ -0,0 +1,13 @@
+const serverless = require("serverless-http");
+const AWS = require("aws-sdk");
+
+// setup db
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const db = { dynamoDb, users: USERS_TABLE };
+
+// create app
+const app = require("app")(db);
+
+// export to serverless
+module.exports.handler = serverless(app);
