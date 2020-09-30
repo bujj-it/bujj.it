0c345181e1212c0b280cb532fa0f49e1570648ae
Squareben1 <bengittins@live.com>
2020-09-30 17:07:33 +0100

    POST /api/spending-plans - successful spendingPlan creation - 1 expense

--- server/app/controllers/spendingPlans.controller.js
+++ server/app/controllers/spendingPlans.controller.js
@@ -1,5 +1,32 @@
+const debug = require("debug")("express:error:spendingPlansController");
+
 module.exports = (db) => {
-  const create = async (req, res) => res.status(200);
+  const database = db.dynamoDb;
+  const userTable = db.users;
+
+  const create = async (req, res) => {
+    try {
+      const createUserSpendingPlanParams = {
+        TableName: userTable,
+        Key: { userId: req.userId },
+        UpdateExpression: "SET spendingPlan = :newSpendingPlan",
+        ExpressionAttributeValues: {
+          ":newSpendingPlan": {
+            income: req.body.income,
+            expenses: req.body.expenses,
+            saving_percentage: req.body.saving_percentage,
+          },
+        },
+      };
+      await database.update(createUserSpendingPlanParams).promise();
+      res.status(200).send({
+        message: "New Spending Plan created.",
+      });
+    } catch (err) {
+      debug(err);
+      res.status(500).send({ message: "Something went wrong!" });
+    }
+  };
 
   return {
     create,
--- server/spec/endpoints/spendingPlans.spec.js
+++ server/spec/endpoints/spendingPlans.spec.js
@@ -1,17 +1,66 @@
 // setup environment
-require('spec/specHelper');
+require("spec/specHelper");
+
+// require helpers
+const { getAccessToken, testUser } = require("spec/helpers/usersHelper");
 
 // create app
-const db = require('spec/dbSetup');
-const app = require('app')(db);
-const request = require('supertest')(app);
+const db = require("spec/dbSetup");
+const app = require("app")(db);
+const request = require("supertest")(app);
 
-describe('spendingPlans endpoint', () => {
-  describe('POST /api/spending-plans', () => {
-    test('no login token', async () => {
-      const response = await request.post('/api/spending-plans');
+beforeEach(async () => {
+  await db.dynamoDb
+    .put({
+      TableName: db.users,
+      Item: testUser,
+    })
+    .promise();
+});
+
+afterEach(async () => {
+  await db.dynamoDb
+    .delete({
+      TableName: db.users,
+      Key: {
+        userId: testUser.userId,
+      },
+    })
+    .promise();
+});
+
+describe("spendingPlans endpoint", () => {
+  describe("POST /api/spending-plans", () => {
+    let accessToken;
+
+    beforeEach(async () => {
+      accessToken = await getAccessToken();
+    });
+
+    test("no login token", async () => {
+      const response = await request.post("/api/spending-plans");
       expect(response.status).toBe(403);
-      expect(response.body.message).toBe('No token provided!');
+      expect(response.body.message).toBe("No token provided!");
+    });
+
+    test("successful spendingPlan creation - 1 expense", async () => {
+      const testSpendingPlan = {
+        income: 1000,
+        expenses: { 1: { name: "rent", value: 500 } },
+        saving_percentage: 10,
+      };
+      const response = await request
+        .post("/api/spending-plans")
+        .set("cookie", accessToken)
+        .send(testSpendingPlan);
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBe("New Spending Plan created.");
+      const checkSpendingPlanParams = {
+        Key: { userId: testUser.userId },
+        TableName: db.users,
+      };
+      const result = await db.dynamoDb.get(checkSpendingPlanParams).promise();
+      expect(result.Item.spendingPlan).toEqual(testSpendingPlan);
     });
   });
 });
