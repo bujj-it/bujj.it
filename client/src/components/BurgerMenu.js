190f45c1b88acd59dc81c5141efb344f783928e4
cpcwood <cpcwood@outlook.com>
2020-12-06 16:13:17 +0000

    fix hamburger menu onclick event to use react hooks

--- client/src/components/BurgerMenu.js
+++ client/src/components/BurgerMenu.js
@@ -1,37 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BurgerMenu = () => {
-  window.addEventListener("load", function () {
-    var hamburgerContainers = document.getElementsByClassName(
-      "hamburger_container"
-    );
-    function addAnimation() {
-      this.classList.remove("reset_animation");
-      this.classList.add("clicked");
-      this.removeEventListener("click", addAnimation);
-      this.addEventListener("click", addReverseAnimation);
-    }
-    function addReverseAnimation() {
-      this.classList.remove("clicked");
-      this.classList.add("reset_animation");
-      this.removeEventListener("click", addReverseAnimation);
-      this.addEventListener("click", addAnimation);
-    }
-    for (var i = 0; i < hamburgerContainers.length; i++) {
-      hamburgerContainers[i].addEventListener("click", addAnimation);
-    }
-  });
+  const [clicked, setClicked] = useState(null);
+
+  let clickedClass
+  if (clicked === true) {
+    clickedClass = 'clicked'
+  } else if (clicked === false) {
+    clickedClass = "reset_animation"
+  } else {
+    clickedClass = ''
+  }
 
   return (
-    <>
-      <div className="hamburger_container">
-        <div className="hamburger">
-          <div className="hamburger_bar" id="hamburger_crown"></div>
-          <div className="hamburger_bar" id="hamburger_patty"></div>
-          <div className="hamburger_bar" id="hamburger_heel"></div>
-        </div>
+    <div className={`hamburger_container ${clickedClass}`} onClick={() => setClicked(!clicked)}>
+      <div className="hamburger">
+        <div className="hamburger_bar" id="hamburger_crown"></div>
+        <div className="hamburger_bar" id="hamburger_patty"></div>
+        <div className="hamburger_bar" id="hamburger_heel"></div>
       </div>
-    </>
+    </div>
   );
 };
 
