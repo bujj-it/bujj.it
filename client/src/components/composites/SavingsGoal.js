a1851e4c94b276e1f9563b56ca959bb98b1becee
cpcwood <cpcwood@outlook.com>
2021-01-28 18:09:55 +0000

    scroll to next section

--- client/src/components/composites/SavingsGoal.js
+++ client/src/components/composites/SavingsGoal.js
@@ -1,10 +1,8 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import { connect } from 'react-redux';
-import nextBudgetFlowSectionHelper from '../../stateHelpers/nextBudgetFlowSectionHelper' 
 
 import {isCurrentSectionSelector} from '../../selectors/budgetFlowSelectors'
 const currentBudgetFlowSection = 'SAVINGS_GOAL'
-const nextBudgetFlowSection = nextBudgetFlowSectionHelper(currentBudgetFlowSection)
 
 const mapStateToProps = state => {
   return {
@@ -14,13 +12,23 @@ const mapStateToProps = state => {
 
 const SavingsGoal = props => {
 
-  const savingsGoal = props.isCurrentSection ? (
-    <section className='section-container savings-goal-container'>
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    window.scroll({
+      top: sectionRef.current.getBoundingClientRect().top,
+      left: 0, 
+      behavior: 'smooth'
+    });
+  })
+
+  const visible = props.isCurrentSection ? 'visible' : ''
+
+  return (
+    <section ref={sectionRef} className={`section-container savings-goal-container ${visible}`}>
       ENTER SAVINGS GOAL
     </section>
-  ) : ''
-
-  return savingsGoal
+  )
 }
 
 export default connect(mapStateToProps)(SavingsGoal);
