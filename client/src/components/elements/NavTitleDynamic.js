50f1b54844e30e73e4610968837d06013c963a77
cpcwood <cpcwood@outlook.com>
2021-02-09 13:28:28 +0000

    update dynamic header and glassy panes

--- client/src/components/elements/NavTitleDynamic.js
+++ client/src/components/elements/NavTitleDynamic.js
@@ -4,6 +4,15 @@ const NavTitleDynamic = () => {
 
   const navTitleRef = useRef(null)
 
+  const [loadInitialStyles] = useState(null);
+
+  const [isHeaderMinimised, _setIsHeaderMinimised] = useState(false);
+  const isHeaderMinimisedRef = React.useRef(isHeaderMinimised);
+  const setIsHeaderMinimised = data => {
+    isHeaderMinimisedRef.current = data;
+    _setIsHeaderMinimised(data);
+  };
+
   const [targetFontSize, _setTargetFontSize] = useState(null);
   const targetFontSizeRef = React.useRef(targetFontSize);
   const setTargetFontSize = data => {
@@ -25,27 +34,33 @@ const NavTitleDynamic = () => {
     _setInitialFontSize(data);
   };
 
-  const [loadInitialStyles] = useState(null);
+  
 
   const handleScroll = () => {
     const scrollTop = window.scrollY
-    const newPadding = Math.max(0, initialPaddingRef.current - scrollTop)
+    const newPadding = Math.max(0, initialPaddingRef.current - (scrollTop / 3))
 
-    const fontSizeDifference = initialFontSizeRef.current - targetFontSizeRef.current
-    const sizeRatio = newPadding / initialPaddingRef.current
-    const newFontSize = Math.floor(sizeRatio * fontSizeDifference) + targetFontSizeRef.current
-
-    requestAnimationFrame(() => {
+    if (!isHeaderMinimisedRef.current || newPadding) {
+      const sizeRatio = newPadding / initialPaddingRef.current
+      const fontSizeDifference = initialFontSizeRef.current - targetFontSizeRef.current
+      const newFontSize = Math.trunc(sizeRatio * fontSizeDifference) + targetFontSizeRef.current
       const navTitleElement = navTitleRef.current
-      navTitleElement.style.paddingTop = `${newPadding}px`;
-      navTitleElement.style.fontSize = `${newFontSize}px`
-    })
+  
+      window.requestAnimationFrame(() => {
+        navTitleElement.style.paddingTop = `${newPadding}px`;
+        navTitleElement.style.paddingBottom = `${newPadding}px`;
+        navTitleElement.style.fontSize = `${newFontSize}px`
+      })
+      if (newPadding === 0) {
+        setIsHeaderMinimised(true)
+      }
+    }
   }
 
   useEffect(() => {
     const navTitleElement = navTitleRef.current
     const navTitleStyle = getComputedStyle(navTitleElement) 
-    const navBarStyle = getComputedStyle(navTitleElement.parentElement.parentElement.parentElement)
+    const navBarStyle = getComputedStyle(navTitleElement.parentElement)
 
     setTargetFontSize(parseInt(navBarStyle.fontSize, 10))
     setInitialPadding(parseInt(navTitleStyle.paddingTop, 10))
