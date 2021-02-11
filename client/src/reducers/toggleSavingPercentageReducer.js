1d7f02b99f53ed87a628d6f7279378c948ce5cf1
cpcwood <cpcwood@outlook.com>
2021-02-11 16:19:37 +0000

    updating income and expenses resets saving percentage toggle

--- client/src/reducers/toggleSavingPercentageReducer.js
+++ client/src/reducers/toggleSavingPercentageReducer.js
@@ -1,9 +1,17 @@
-import { TOGGLE_SAVING_PERCENTAGE } from 'constants/actionTypes.js';
+import { TOGGLE_SAVING_PERCENTAGE, UPDATE_INCOME, UPDATE_EXPENSE, REMOVE_EXPENSE } from 'constants/actionTypes.js';
 
-export default (state = false, action) => {
+const defaultToggleState = false 
+
+export default (state = defaultToggleState, action) => {
   switch (action.type) {
     case TOGGLE_SAVING_PERCENTAGE:
       return !!action.payload;
+    case UPDATE_INCOME:
+      return defaultToggleState;
+    case UPDATE_EXPENSE:
+      return defaultToggleState;
+    case REMOVE_EXPENSE:
+      return defaultToggleState;
     default:
       return state;
   }
